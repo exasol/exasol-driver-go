b09a13b0eca5f9f850b784d048f543abfda3cd59
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-03-13 07:52:51 +0100

    Upgrade default Exasol version for testing

--- pkg/integrationTesting/dbTestSetup.go
+++ pkg/integrationTesting/dbTestSetup.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-const defaultExasolDbVersion = "8.23.0"
+const defaultExasolDbVersion = "8.24.0"
 
 type DbTestSetup struct {
 	suite          *suite.Suite
