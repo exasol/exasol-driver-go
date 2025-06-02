96db93be6b901e29123bdaf80bfed31e75bf4d58
Christoph Pirkl <christoph.pirkl@exasol.com>
2025-06-02 13:18:46 +0200

    Upgrade golangci-lint

--- examples/simple.go
+++ examples/simple.go
@@ -3,8 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/exasol/exasol-driver-go"
 	"log"
+
+	"github.com/exasol/exasol-driver-go"
 )
 
 func main() {
@@ -21,13 +22,17 @@ func main() {
 	err = database.Ping()
 	onError(err)
 
-	database.Exec("CREATE SCHEMA IF NOT EXISTS my_schema")
-	database.Exec("OPEN SCHEMA my_schema")
+	_, err = database.Exec("CREATE SCHEMA IF NOT EXISTS my_schema")
+	onError(err)
+
+	_, err = database.Exec("OPEN SCHEMA my_schema")
+	onError(err)
+
 	_, err = database.Exec("CREATE OR REPLACE TABLE my_schema.CUSTOMERS (ref_id int , b VARCHAR(20)) ")
 	onError(err)
 
-	result, err := database.Exec(fmt.Sprintf(`IMPORT INTO my_schema.CUSTOMERS FROM LOCAL CSV FILE './data.csv' COLUMN SEPARATOR = ';' ENCODING = 'UTF-8'
-ROW SEPARATOR = 'LF'`))
+	result, err := database.Exec(`IMPORT INTO my_schema.CUSTOMERS FROM LOCAL CSV FILE './data.csv' COLUMN SEPARATOR = ';' ENCODING = 'UTF-8'
+ROW SEPARATOR = 'LF'`)
 	onError(err)
 	log.Println(result.RowsAffected())
 	rows, err := database.Query("SELECT * FROM my_schema.CUSTOMERS")
