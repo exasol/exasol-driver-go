717b517689fbcc3e565a6ae2e059ea351ae62dd2
ckunki <christoph.kuhnke@exasol.com>
2026-09-22 08:20:09 +0200

    Made constants private
    and added comment regarding sign of integers

--- pkg/connection/column_size.go
+++ pkg/connection/column_size.go
@@ -5,9 +5,9 @@ import (
 )
 
 const (
-	DecimalSizeInt32 = 10
-	DecimalSizeInt64 = 19
-	VarCharLength    = 2000000
+	decimalSizeInt32 = 10
+	decimalSizeInt64 = 19
+	varCharLength    = 2000000
 )
 
 // Return the number of decimal digits required to represent a signed integer
@@ -15,7 +15,10 @@ const (
 //
 // The return value can be used to compute the size for SQL data type DECIMAL
 // as stored in the constants above.
-func numberOfDigits(numberOfBits int) (digits int) {
-	n := numberOfBits - 1
+func numberOfDigits(bitCount int) (digits int) {
+	// The number of digits in the DECIMAL's scale can ignore one bit
+	// representing the +/- sign of the signed integer as the SQL data type
+	// stores the sign separately.
+	n := bitCount - 1
 	return int(math.Ceil(float64(n) * math.Ln2 / math.Ln10))
 }
--- pkg/connection/column_size_test.go
+++ pkg/connection/column_size_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestColumnSizes(t *testing.T) {
-	assert.Equal(t, numberOfDigits(32), DecimalSizeInt32)
-	assert.Equal(t, numberOfDigits(64), DecimalSizeInt64)
-	assert.Equal(t, 2000000, VarCharLength)
+	assert.Equal(t, numberOfDigits(32), decimalSizeInt32)
+	assert.Equal(t, numberOfDigits(64), decimalSizeInt64)
+	assert.Equal(t, 2000000, varCharLength)
 }
