9aab29ee915718101ce524660d64033b03fb807a
ckunki <christoph.kuhnke@exasol.com>
2026-09-22 09:04:24 +0200

    Updated function doc comment

--- pkg/connection/column_size.go
+++ pkg/connection/column_size.go
@@ -10,8 +10,8 @@ const (
 	varCharLength    = 2000000
 )
 
-// Return the number of decimal digits required to represent a signed integer
-// with the specified number of bits.
+// numberOfDigits returns the number of decimal digits required to represent a
+// signed integer with the specified number of bits.
 //
 // The return value can be used to compute the size for SQL data type DECIMAL
 // as stored in the constants above.
