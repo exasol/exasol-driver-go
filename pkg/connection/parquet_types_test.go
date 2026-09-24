229b572c184fe24578829a839af4ca94eeda9aaa
Christoph Kuhnke <christoph.kuhnke@exasol.com>
2026-09-24 09:39:59 +0200

    #172: Added support to build a `CREATE TABLE` SQL statement (#173)

--- pkg/connection/parquet_types_test.go
+++ pkg/connection/parquet_types_test.go
@@ -29,6 +29,8 @@ func TestColumnSizes(t *testing.T) {
 	assert.Equal(t, 2000000, maxVarcharLength)
 }
 
+const invalidPhysicalDataType = 33
+
 var mapPhysicalTypeTests = []struct {
 	physical      parquet.Kind
 	size          int64
@@ -47,12 +49,12 @@ var mapPhysicalTypeTests = []struct {
 	{parquet.ByteArray, 1, varcharColumn(maxVarcharLength), ""},
 	{parquet.FixedLenByteArray, 1, varcharColumn(1), ""},
 	{parquet.FixedLenByteArray, 123, varcharColumn(123), ""},
-	{parquet.FixedLenByteArray, maxVarcharLength + 1, "", "exceeds supported maxiumum"},
+	{parquet.FixedLenByteArray, maxVarcharLength + 1, "", "exceeds supported maximum"},
 	{parquet.Float, 0, doublePrecisionColumn, ""},
 	{parquet.Float, 1, doublePrecisionColumn, ""},
 	{parquet.Double, 0, doublePrecisionColumn, ""},
 	{parquet.Double, 1, doublePrecisionColumn, ""},
-	{33, 0, "", "unsupported Parquet physical data type"},
+	{invalidPhysicalDataType, 0, "", "unsupported Parquet physical data type"},
 }
 
 func TestMapPhysicalType(t *testing.T) {
@@ -63,6 +65,87 @@ func TestMapPhysicalType(t *testing.T) {
 			if tt.expectedError == "" {
 				assert.NoError(t, err, "mapPhysicalType failed unexpectedly")
 				assert.Equal(t, result, tt.expectedType)
+			} else {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tt.expectedError)
+			}
+		})
+	}
+}
+
+var createTableStatementTests = []struct {
+	name          string
+	columns       []parquetColumn
+	expected      string
+	expectedError string
+}{
+	{"empty_list", make([]parquetColumn, 0), "", "empty list of columns"},
+	{
+		"decimal_10", []parquetColumn{{[]string{"d1"}, parquet.Int32, 0}},
+		"(\"d1\" DECIMAL(10,0))", "",
+	},
+	{
+		"decimal_19", []parquetColumn{{[]string{"d2"}, parquet.Int64, 0}},
+		"(\"d2\" DECIMAL(19,0))", "",
+	},
+	{
+		"timestamp", []parquetColumn{{[]string{"ts"}, parquet.Int96, 0}},
+		"(\"ts\" TIMESTAMP(9))", "",
+	},
+	{
+		"boolean", []parquetColumn{{[]string{"b"}, parquet.Boolean, 0}},
+		"(\"b\" BOOLEAN)", "",
+	},
+	{
+		"varchar_max", []parquetColumn{{[]string{"v1"}, parquet.ByteArray, 0}},
+		"(\"v1\" VARCHAR(2000000) CHARACTER SET UTF8)", "",
+	},
+	{
+		"varchar_flex", []parquetColumn{{[]string{"v2"}, parquet.FixedLenByteArray, 123}},
+		"(\"v2\" VARCHAR(123) CHARACTER SET UTF8)", "",
+	},
+	{
+		"error_1", []parquetColumn{{[]string{"e1"}, parquet.FixedLenByteArray, maxVarcharLength + 1}},
+		"", "exceeds supported maximum",
+	},
+	{
+		"double_float", []parquetColumn{{[]string{"dp1"}, parquet.Float, 0}},
+		"(\"dp1\" DOUBLE PRECISION)", "",
+	},
+	{
+		"double_double", []parquetColumn{{[]string{"dp2"}, parquet.Double, 0}},
+		"(\"dp2\" DOUBLE PRECISION)", "",
+	},
+	{
+		"error_2", []parquetColumn{{[]string{"e1"}, invalidPhysicalDataType, 0}},
+		"", "unsupported Parquet physical data type",
+	},
+	{
+		"multiple_valid", []parquetColumn{
+			{[]string{"d1"}, parquet.Int64, 0},
+			{[]string{"v1"}, parquet.FixedLenByteArray, 345},
+		},
+		"(\"d1\" DECIMAL(19,0), \"v1\" VARCHAR(345) CHARACTER SET UTF8)",
+		"",
+	},
+	{
+		"multiple_2nd_invalid", []parquetColumn{
+			{[]string{"d1"}, parquet.Int64, 0},
+			{[]string{"e1"}, invalidPhysicalDataType, 0},
+		},
+		"",
+		"unsupported",
+	},
+}
+
+func TestCreateTableStatement(t *testing.T) {
+	for _, tt := range createTableStatementTests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := createTableStatement("S.T", tt.columns)
+			if tt.expectedError == "" {
+				assert.NoError(t, err, "CreateTableStatement failed unexpectedly")
+				expected := fmt.Sprintf("CREATE TABLE S.T %s", tt.expected)
+				assert.Equal(t, result, expected)
 			} else {
 				assert.Error(t, err)
 				assert.Contains(t, err.Error(), tt.expectedError)
